300b2c3ccceebb335bf2431aacbbce0bebc733fc
Lv Ming <lvming@restartech.com>
2022-02-28 17:09:57 +0800

    Add etc and modules symlink

--- extra/bin/start_niagarad.sh
+++ extra/bin/start_niagarad.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-mkdir -p $HOME/etc/niagara $HOME/Niagara4.7/vykon
-chown 5011:5011 -R $HOME/etc/niagara $HOME/Niagara4.7/vykon
+mkdir -p $SNAP_COMMON/etc
+mkdir -p $SNAP_COMMON/modules
+mkdir -p $SNAP_COMMON/Niagara4.7/tridium
+mkdir -p $SNAP_COMMON/Niagara4.7/vykon
+chown 5011:5011 -R $SNAP_COMMON/etc
+chown 5011:5011 -R $SNAP_COMMON/modules
+chown 5011:5011 -R $SNAP_COMMON/Niagara4.7
 exec $SNAP/usr/bin/setpriv --reuid 5011 --regid 5011 --keep-groups -- $SNAP/bin/niagarad $@
